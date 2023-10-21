e7e28fda0113e72341221236b10c6569964c37be
Indra Andrean <https://github.com/IndraAndrean/DDP-MI-23.git>
2023-10-21 18:23:03 +0700

    UTP

--- Documents/DDP-MI-23/04 Perulangan/indra_angka_naik.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int angkanaik;
-    cin >> angkanaik;
-    for(int angka = 1; angka <= angkanaik; angka++){
-        cout << angka << " " << endl;
-    }
-}
